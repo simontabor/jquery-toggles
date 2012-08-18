1f7143ef637bda086041a544472f577b90ab15a4
Simon Tabor <me@simontabor.com>
2012-08-19 00:54:49 +0100

    mixed mouseleave issues

--- toggles.js
+++ toggles.js
@@ -25,7 +25,7 @@
         },o.animtime);
         if (o.checkbox) $(o.checkbox).attr('checked',true);
       }else{
-        //make it inactive
+        // make it inactive
         inner.animate({
           marginLeft: -w+h
         },o.animtime);
@@ -40,13 +40,13 @@
       o.click = opts.click || self;
 
 
-    var h = self.height(), w= self.width();
-    if (h === 0 || w===0) {
-      self.height(h = 20);
-      self.width(w = 50);
-    }
-    var slide = $('<div class="slide" />'), inner = $('<div class="inner" />'),on = $('<div class="on" />'), off = $('<div class="off" />'), blob = $('<div class="blob" />');
-    on
+      var h = self.height(), w= self.width();
+      if (h === 0 || w===0) {
+        self.height(h = 20);
+        self.width(w = 50);
+      }
+      var slide = $('<div class="slide" />'), inner = $('<div class="inner" />'),on = $('<div class="on" />'), off = $('<div class="off" />'), blob = $('<div class="blob" />');
+      on
       .css({
         height:h,
         width: w-h/2,
@@ -55,7 +55,7 @@
         lineHeight: h+'px'
       })
       .text(o.ontext);
-    off
+      off
       .css({
         height:h,
         width: w-h/2,
@@ -65,57 +65,38 @@
         lineHeight: h+'px'
       })
       .text(o.offtext);
-    blob.css({
-      height: h,
-      width: h,
-      marginLeft: -h/2
-    });
-    inner.css({
-      width: w*2-h,
-      marginLeft: (o.on) ? 0 : -w+h
-    });
-    if (o.on) {
-      slide.addClass('active');
-      if (o.checkbox) $(o.checkbox).attr('checked',true);
-    }
-    self.html('');
-    self.append(slide.append(inner.append(on,blob,off)));
-    var diff = 0, time;
-    self.on('toggle', function() {
-      toggle(slide,w,h);
-    });
-    if (o.clickable) {
-      o.click.click(function(e) {
-        if (e.target !=  blob[0] || !o.dragable) {
-          self.trigger('toggle');
-        }      
+      blob.css({
+        height: h,
+        width: h,
+        marginLeft: -h/2
       });
-    }
-    if (o.dragable) {
-      blob.on('mouseup mouseleave',function(e) {
-        self.off('mousemove');
-        if (slide.hasClass('active')) {
-          if (diff < (-w+h)/2) {
-            self.trigger('toggle');
-          }else{
-            inner.animate({
-              marginLeft: 0
-            }, o.animtime/2);
-          }
-        }else{
-          if (diff > (w-h)/2) {
-            self.trigger('toggle');
-          }else{
-            inner.animate({
-              marginLeft: -w+h
-            },o.animtime/2);
-          }
-        }
-        if (((+new Date()-100 < time) || diff===0) && o.clickable && e.type != 'mouseleave') self.trigger('toggle');
+      inner.css({
+        width: w*2-h,
+        marginLeft: (o.on) ? 0 : -w+h
       });
-      blob.on('mousedown',function(e) {
+      if (o.on) {
+        slide.addClass('active');
+        if (o.checkbox) $(o.checkbox).attr('checked',true);
+      }
+      self.html('');
+      self.append(slide.append(inner.append(on,blob,off)));
+      var diff = 0, time;
+      self.on('toggle', function() {
+        toggle(slide,w,h);
+      });
+      if (o.clickable) {
+        o.click.click(function(e) {
+          if (e.target !=  blob[0] || !o.dragable) {
+            self.trigger('toggle');
+          }      
+        });
+      }
+      if (o.dragable) {
+        blob.on('mousedown',function(e) {
+          blob.off('mouseup mouseleave');
           time = +new Date();
           var cursor = e.pageX;
+
           self.on('mousemove',blob,function(e) {
             diff = e.pageX - cursor;
             if (slide.hasClass('active')) {
@@ -128,8 +109,29 @@
               });
             }
           });
-      });
-    }
-  });
+          blob.on('mouseup mouseleave',function(e) {
+            self.off('mousemove');
+            if (slide.hasClass('active')) {
+              if (diff < (-w+h)/2) {
+                self.trigger('toggle');
+              }else{
+                inner.animate({
+                  marginLeft: 0
+                }, o.animtime/2);
+              }
+            }else{
+              if (diff > (w-h)/2) {
+                self.trigger('toggle');
+              }else{
+                inner.animate({
+                  marginLeft: -w+h
+                },o.animtime/2);
+              }
+            }
+            if (((+new Date()-100 < time) || diff===0) && o.clickable && e.type != 'mouseleave') self.trigger('toggle');
+          });
+        });
+      }
+    });
   };
 })( jQuery );
